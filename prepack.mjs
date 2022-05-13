411d863361cb6c7a366c8ebe5317d6bc5c1b1aee
bamboooooo <maxuezhu1119@gmail.com>
2022-05-13 15:27:29 +0800

    add oss upload

--- prepack.mjs
+++ prepack.mjs
@@ -1,8 +1,18 @@
 #!/usr/bin/env zx
 import iconv from 'iconv-lite';
 const configFileStr = await fs.readFile('./lisaiss.iss');
+let manifest = await fs.readFile('./manifest.json');
+manifest = JSON.parse(manifest.toString());
+let zephyrVersion; 
+for (let i in manifest) {
+    const plugin = manifest[i];
+    console.log(plugin)
+    if (plugin.name === '@lisa-plugin/zephyr') {
+      zephyrVersion = plugin.version
+  }
+}
 const pwd = path.join(process.cwd(), 'node', '*')
 const zephyrPwd = path.join(os.homedir(), '.listenai', 'lisa-zephyr', '*');
 var str = iconv.decode(configFileStr, 'GB2312');
-const result = str.replace(/###pwd###/g, pwd).replace(/###zephyrPwd###/g, zephyrPwd);
+const result = str.replace(/###zephyrVersion###/g, zephyrVersion||'').replace(/###pwd###/g, pwd).replace(/###zephyrPwd###/g, zephyrPwd);
 await  fs.writeFile('./lisaiss.iss', iconv.encode(result, 'GB2312'));
