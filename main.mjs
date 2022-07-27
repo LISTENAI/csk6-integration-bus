a664890126f4201b908aa0fcfb1dbf5e6af8612c
jw <105700413+listenai-jw@users.noreply.github.com>
2022-07-27 11:20:00 +0800

    (del) remove redunatant code (add) os env

--- main.mjs
+++ main.mjs
@@ -3,14 +3,6 @@
 let manifest = await fs.readFile('./manifest.json');
 manifest = JSON.parse(manifest.toString());
 
-// for (let i in manifest) {
-//   const plugin = manifest[i];
-  // await lpmInstall(plugin.name, plugin.version);
 await $`lisa install @lisa-plugin/zephyr@1.6.2-beta.7 -g  --registry=https://registry-lpm.listenai.com `
 await $`lisa install @lisa-plugin/term@1.1.0 -g  --registry=https://registry-lpm.listenai.com `
-// }
 await $`lisa zep use-env csk6`
-
-// async function lpmInstall(name, version) {
-//   await $`lisa install ${name}@${version} -g  --registry=https://registry-lpm.listenai.com `
-// }
