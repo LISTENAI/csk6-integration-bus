7069270b2edae6ea85832291ef19153702dedbe1
jw <105700413+listenai-jw@users.noreply.github.com>
2022-08-03 10:32:51 +0800

    lisa-zephyr version update (#15)

--- main.mjs
+++ main.mjs
@@ -3,6 +3,6 @@
 let manifest = await fs.readFile('./manifest.json');
 manifest = JSON.parse(manifest.toString());
 
-await $`lisa install @lisa-plugin/zephyr@1.6.2-beta.9 -g  --registry=https://registry-lpm.listenai.com `
+await $`lisa install @lisa-plugin/zephyr@1.6.2-beta.11 -g  --registry=https://registry-lpm.listenai.com `
 await $`lisa install @lisa-plugin/term@1.1.0 -g  --registry=https://registry-lpm.listenai.com `
 await $`lisa zep use-env csk6 `
