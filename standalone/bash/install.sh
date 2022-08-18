afea67d874919ac63abebfce40b4cd40fbfa8bdd
jw <105700413+listenai-jw@users.noreply.github.com>
2022-08-18 14:56:21 +0800

    csk6-integration-bus for 1.6.2+ (#23)
    * Develop (#8)
    
    * (mod) convert encoding of lisaiss.iss to utf-8
    (mod) use 1.6.2-beta.6 zephyr
    
    * (add) res pack download
    (add) integrity check
    
    * (add) workspace init
    (add) integrity check
    
    * (add) progress bar for unzip
    
    * (mod) re-enable progress bar for download
    
    * (add) os env to /etc/environment
    (add) mklink lisa
    
    * (add) channel option for testing
    (add) hard-coded csk6 env on init setup
    
    * (add) install xz-utils
    
    * pack env package
    
    * for debug purpose
    
    * apply lisa_home os var
    
    * (del) remove redunatant code
    (add) os env
    
    * (add) manually install csk6 env
    (add) user-script housekeeping
    
    * fix 2
    
    * list packages for debug purpose
    
    * fix 3
    
    * fix 4 debug
    
    * fix 6
    
    * user-script fix 1
    
    * add git as dependency
    
    * remove debug purpose things
    
    * add git as dependency
    
    * remove debug purpose things
    
    * Feature/wheel package distribution (#9)
    
    * (add) lisa zephyr requirements
    (add) action config file
    
    * fix config file
    
    * use zip
    
    * fix zip cmd
    
    * use 7z
    
    * fix merge packages
    
    * fix path error
    
    * use find to find all 7z
    
    * fix 1
    
    * fix 2
    
    * (add) remove intermediate packages
    
    * change another plugin
    
    * change structure
    
    * (add) local requirements
    
    * fix path
    
    * fix cmd
    
    * Feature/use 7z res pack (#10)
    
    * (mod) lisa-zephyr-v1.6.2-beta.9
    (mod) use 7z to decompress
    
    * run action on develop/master only
    
    * (mod) update requirements
    (mod) sort functions
    
    * remove local debug mode override (#11)
    
    * Feature/limited support for macos (#12)
    
    * (add) brew install
    (add) test workflow
    
    * fix yml
    
    * (add) +x for script
    
    * use which instead of type for checking commands
    
    * sep linux/darwin in install script
    
    * case thing
    
    * case thing 2
    
    * sep sed cmd in linux/darwin
    
    * change output
    
    * Feature/block inst on part failure (#13)
    
    * (add) mark inst as fail if unable to decompress or download
    
    * (add) also run inst test on macos-12
    
    * remove debug override
    
    * Update mactest.yml
    
    * lisa-zephyr version update (#15)
    
    * (mod) use readlink
    
    * Feature/whl requirements update (#17)
    
    * (add) enquiries
    (mod) pyocd-0.34.2.dev20
    
    * (del) remove on push on mactest workflow
    
    * change mactest on
    
    * set install channel to beta by default (#18)
    
    * (mod) seperate requirement file for ci and end-user (#19)
    
    (mod) use ci_requirement in workflow
    
    * (del) remove reduntant requirements, let end-user download them (#20)
    
    (mod) remove readlink, seems not matter a lot
    
    * setuptools
    
    * fix requirements
    
    * (mod) use v1.6.2 lisa-plugin-zephyr (#21)
    
    * Runner/use zgsz runner (#22)
    
    * use local runner
    
    * (del) temp disable mac runner
    (mod) also use zgsz-01 runner in test
    
    * re-enable mac
    
    * (mod) end-user inst script default channel -> stable
    
    * test with hosted runner
    
    * (mod) completely remove node cleanup
    (mod) use local download on downloading node
    
    * seperate node download domain
    
    * change node domain
    
    * revert
    
    * enable multithreading on xz compressing
    
    * temp rollback
    
    * (add) register mac runner
    (mod) use mac runner test
    
    * multi-thread
    
    * (add) remove pre-installed node
    (del) mac test script
    
    * (mod) remove node only if it exists
    
    * (mod) change node removal cmd
    
    * use local cache
    
    * fix workflow
    
    * use internal cache manager
    
    * checkout cache lib then use
    
    * set access token
    
    * use v1.0.1 cache manager
    
    * use 1.0.2 cache manager
    
    * add port
    
    * use private key
    
    * use cache manager 1.0.3
    
    * use password only
    
    * fix remove node
    
    * resume oss

--- standalone/bash/install.sh
+++ standalone/bash/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 lisa_has() {
-  type "$1" > /dev/null 2>&1
+  which "$1" > /dev/null 2>&1
 }
 
 lisa_default_install_dir() {
@@ -15,6 +15,11 @@ lisa_echo() {
 lisa_download() {
   if lisa_has "curl"; then
     command curl --fail --compressed -q "$@"
+    local CURL_ERR=$?
+    if [ $CURL_ERR -ne 0 ]; then
+      lisa_echo >&2 "*** Erm.... unable to download the archive, error = ${CURL_ERR}"
+      exit 1
+    fi
   elif lisa_has "wget"; then
     ARGS=$(lisa_echo "$@" | command sed -e 's/--progress-bar /--progress=bar /' \
                             -e 's/--compressed //' \
@@ -26,14 +31,39 @@ lisa_download() {
                             -e 's/-o /-O /' \
                             -e 's/-C - /-c /')
     eval wget $ARGS
+    local WGET_ERR=$?
+    if [ $WGET_ERR -ne 0 ]; then
+      lisa_echo >&2 "*** Erm.... unable to download the archive, error = ${WGET_ERR}"
+      exit 1
+    fi
+  else
+    lisa_echo >&2 "*** You need either curl or wget to download packages"
+    exit 1
   fi
 }
 
 lisa_tar() {
   if lisa_has "tar"; then
-    command tar xf "$@"
+    command pv "$1" |tar xJf - -C "$2"
+    if [ $? -ne 0 ]; then
+      lisa_echo >&2 '*** Erm.... unable to decompress the archive, something went wrong?'
+      exit 1
+    fi
   else
-    lisa_echo >&2 'You need tar to install Lisa'
+    lisa_echo >&2 '*** You need tar to install Lisa'
+    exit 1
+  fi
+}
+
+lisa_un7z() {
+  if lisa_has "7z"; then
+    command 7z x "$1" -o"$2"
+    if [ $? -ne 0 ]; then
+      lisa_echo >&2 '*** Erm.... unable to decompress the archive, something went wrong?'
+      exit 1
+    fi
+  else
+    lisa_echo >&2 '*** You need 7z to install Lisa'
     exit 1
   fi
 }
@@ -41,7 +71,7 @@ lisa_tar() {
 lisa_shell_command_link() {
   lnpath="/usr/local/bin/lisa"
   if [ -L "$lnpath" ]; then
-    rm $lnpath;
+    sudo rm -f $lnpath;
   fi
   lisa_echo "=> ${LISA_BIN}/lisa -> $lnpath"
   sudo ln -s "${LISA_BIN}/lisa" $lnpath
@@ -69,7 +99,81 @@ lisa_get_format() {
   lisa_echo "${LISA_FORMAT-}"
 }
 
+lisa_inst_requirements() {
+  local LISA_OS
+  LISA_OS="$(lisa_get_os)"
+  case "${LISA_OS}" in
+    linux)
+      if lisa_has "apt"; then
+        sudo apt install -y gpg p7zip-full pv xz-utils git
+        if [ $? -ne 0 ]; then
+          lisa_echo >&2 "*** Oops...something went wrong when installing required application(s)"
+          exit 1
+        fi
+      elif lisa_has "yum"; then
+        sudo yum install -y epel-release
+        if [ $? -ne 0 ]; then
+          lisa_echo >&2 "*** Oops...something went wrong when enabling EPEL repository"
+          exit 1
+        fi
+        sudo yum install -y gpg p7zip-full pv xz git
+        if [ $? -ne 0 ]; then
+          lisa_echo >&2 "*** Oops...something went wrong when installing required application(s)"
+          exit 1
+        fi
+      else
+        lisa_echo >&2 "*** No apt/yum found in your system, please install one of them first."
+        exit 1
+      fi
+      ;;
+    darwin)
+      if lisa_has "brew"; then
+        brew install gnupg p7zip pv
+        if [ $? -ne 0 ]; then
+          lisa_echo >&2 "*** Oops...something went wrong when installing required application(s)"
+          exit 1
+        fi
+      else
+        lisa_echo >&2 "*** No brew found in your system, please install one of them first."
+        exit 1
+      fi
+      ;;
+    *)
+      lisa_echo >&2 "*** Not a supported system"
+      exit 1
+  esac
+}
+
+lisa_is_gpgkey_imported() {
+  gpg --list-keys |grep "6DE025312AE473230DA39108E9092D29A4E4A547" >/dev/null 2>&1
+  return $?
+}
+
+lisa_verify_signature() {
+  gpg --verify "$@" >/dev/null 2>&1
+  return $?
+}
+
+lisa_root_check() {
+  if [ $(id -u) -eq 0 ]; then
+    lisa_echo >&2 "*** Please run installer with non-root user"
+    exit 1
+  fi
+}
+
+lisa_channel_selection() {
+  DOWNLOAD_CHANNEL="stable"
+  if [ $# -eq 1 ]; then
+    case "$1" in
+      beta | stable) DOWNLOAD_CHANNEL=$1 ;;
+      *) DOWNLOAD_CHANNEL="stable" ;;
+    esac
+  fi
+}
+
 lisa_do_install() {
+  lisa_echo "Using channel ${DOWNLOAD_CHANNEL}"
+
   local INSTALL_DIR
   INSTALL_DIR="$(lisa_default_install_dir)"
 
@@ -80,24 +184,116 @@ lisa_do_install() {
   LISA_FORMAT="$(lisa_get_format)"
 
   local LISA_SOURCE
-  LISA_SOURCE="https://cdn.iflyos.cn/public/cskTools/lisa-zephyr-${LISA_OS}_x64${LISA_FORMAT}"
+  case "${DOWNLOAD_CHANNEL}" in
+    beta) LISA_SOURCE="https://cdn.iflyos.cn/public/cskTools/lisa-zephyr-${LISA_OS}_x64-${DOWNLOAD_CHANNEL}${LISA_FORMAT}";;
+    *) LISA_SOURCE="https://cdn.iflyos.cn/public/cskTools/lisa-zephyr-${LISA_OS}_x64${LISA_FORMAT}";;
+  esac
 
   local LISA_BIN
-  LISA_BIN="${INSTALL_DIR}/libexec"
+  LISA_BIN="${INSTALL_DIR}/libexec" ``
 
   local LISA_RC
   LISA_RC="${HOME}/ifly/lisa/standalone/bash/.lisarc"
+
+  local LISA_SDK_SOURCE
+  LISA_SDK_SOURCE="https://cdn.iflyos.cn/public/lisa-zephyr-dist/lisa-zephyr-sdk-latest.7z"
+
+  local LISA_SDK_SIG
+  LISA_SDK_SIG="https://cdn.iflyos.cn/public/lisa-zephyr-dist/lisa-zephyr-sdk-latest.7z.sig"
+
+  local LISA_WHL_SOURCE
+  LISA_WHL_SOURCE="https://cdn.iflyos.cn/public/lisa-zephyr-dist/lisa-zephyr-whl-latest.7z"
+
+  local LISA_WHL_SIG
+  LISA_WHL_SIG="https://cdn.iflyos.cn/public/lisa-zephyr-dist/lisa-zephyr-whl-latest.7z.sig"
 
   if ! [ -d "${INSTALL_DIR}" ]; then
     command mkdir -p $INSTALL_DIR
   fi
 
-  lisa_echo "=> Downloading Lisa to '$INSTALL_DIR'"
-  lisa_download -s "$LISA_SOURCE" -o "$INSTALL_DIR/lisa-zephyr-${LISA_OS}_x64${LISA_FORMAT}"
-  lisa_echo "=> tar to '$INSTALL_DIR'"
-  lisa_tar "$INSTALL_DIR/lisa-zephyr-${LISA_OS}_x64${LISA_FORMAT}" -C "$INSTALL_DIR"
+  lisa_echo "=> Installing 7z & gpg"
+  lisa_inst_requirements
+
+  lisa_is_gpgkey_imported
+  local GPGCHECK=$?
+
+  lisa_echo "=> Downloading LISA"
+  lisa_download --progress-bar "$LISA_SOURCE" -o "$INSTALL_DIR/lisa-zephyr-${LISA_OS}_x64${LISA_FORMAT}"
+  lisa_echo "=> Downloading SDK package"
+  lisa_download --progress-bar "$LISA_SDK_SOURCE" -o "$INSTALL_DIR/lisa-zephyr-sdk-latest.7z"
+  lisa_echo "=> Downloading required python wheel package"
+  lisa_download --progress-bar "$LISA_WHL_SOURCE" -o "$INSTALL_DIR/lisa-zephyr-whl-latest.7z"
+
+  if [ $GPGCHECK -eq 0 ]; then
+    lisa_echo "=> Checking integrity of resource package"
+    lisa_download -s "$LISA_SDK_SIG" -o "$INSTALL_DIR/lisa-zephyr-sdk-latest.7z.sig"
+    lisa_download -s "$LISA_WHL_SIG" -o "$INSTALL_DIR/lisa-zephyr-whl-latest.7z.sig"
+    lisa_verify_signature "$INSTALL_DIR/lisa-zephyr-sdk-latest.7z.sig" "$INSTALL_DIR/lisa-zephyr-sdk-latest.7z"
+    local SDK_SIG_OK=$?
+    lisa_verify_signature "$INSTALL_DIR/lisa-zephyr-whl-latest.7z.sig" "$INSTALL_DIR/lisa-zephyr-whl-latest.7z"
+    local WHL_SIG_OK=$?
+    if [ $SDK_SIG_OK -ne 0 ] || [ $WHL_SIG_OK -ne 0 ]; then
+      lisa_echo >&2 "*** Resource packages integrity check failed!"
+      exit 1
+    fi
+  fi
+
+  lisa_echo "=> Extracting LISA to '$INSTALL_DIR'"
+  lisa_tar "$INSTALL_DIR/lisa-zephyr-${LISA_OS}_x64${LISA_FORMAT}" "$INSTALL_DIR"
+  mkdir -p "$INSTALL_DIR/../lisa-zephyr"
+  mv "$INSTALL_DIR/packages" "$INSTALL_DIR/../lisa-zephyr"
+  lisa_echo "=> Extracting SDK package"
+  mkdir -p "$INSTALL_DIR/../csk-sdk"
+  lisa_un7z "$INSTALL_DIR/lisa-zephyr-sdk-latest.7z" "$INSTALL_DIR/../csk-sdk"
+  lisa_echo "=> Extracting WHL package"
+  mkdir -p "$INSTALL_DIR/../lisa-zephyr/whl"
+  lisa_un7z "$INSTALL_DIR/lisa-zephyr-whl-latest.7z" "$INSTALL_DIR/../lisa-zephyr/whl"
+  mv "$INSTALL_DIR/../lisa-zephyr/whl/dependencies/local_requirements.txt" "$INSTALL_DIR/../lisa-zephyr/whl/local_requirements.txt"
+
+  lisa_echo "=> Preparing workspace specially for you"
   lisa_shell_command_link
+  export LISA_HOME=$INSTALL_DIR/../
+  export LISA_PREFIX=$INSTALL_DIR
+  $LISA_HOME/lisa/libexec/lisa zep install
+  if [ $? -ne 0 ]; then
+    lisa_echo >&2 '*** Unable to initialize virtual developing environment'
+    exit 2
+  fi
+  echo "{\"env\":\"csk6\"}" |tee $LISA_HOME/lisa-zephyr/config.json >/dev/null 2>&1
+  $LISA_HOME/lisa/libexec/lisa zep use-sdk "$LISA_HOME/csk-sdk"
+  if [ $? -ne 0 ]; then
+    lisa_echo >&2 '*** Unable to initialize west workspace'
+    exit 2
+  fi
+
+  case "${LISA_OS}" in
+    linux)
+      sudo sed -i '/^LISA_HOME=/d' /etc/environment
+      sudo sed -i '/^LISA_PREFIX=/d' /etc/environment
+      ;;
+    darwin)
+      sudo sed -i '' -e '/^LISA_HOME=/d' /etc/environment
+      sudo sed -i '' -e '/^LISA_PREFIX=/d' /etc/environment
+      ;;
+    *)
+      lisa_echo "Unable to add OS environment, you might need to add it by yourself."
+  esac
+
+  sudo touch /etc/environment
+  echo "LISA_HOME=\"${LISA_HOME}\"" |sudo tee -a /etc/environment >/dev/null 2>&1
+  echo "LISA_PREFIX=\"${LISA_PREFIX}\"" |sudo tee -a /etc/environment >/dev/null 2>&1
+  source /etc/environment
+
+  lisa_echo "=> Some housekeeping"
+  rm -f "$INSTALL_DIR/lisa-zephyr-linux_x64.tar.xz"
+  rm -f "$INSTALL_DIR/lisa-zephyr-sdk-latest.7z"
+  rm -f "$INSTALL_DIR/lisa-zephyr-whl-latest.7z"
+  rm -f "$INSTALL_DIR/lisa-zephyr-sdk-latest.7z.sig"
+  rm -f "$INSTALL_DIR/lisa-zephyr-whl-latest.7z.sig"
+  
   lisa_echo "=> Success! try run command 'lisa info zephyr'"
 }
 
+lisa_root_check
+lisa_channel_selection $1
 lisa_do_install
