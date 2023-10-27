2c617c69ae94ba99b88cd0c124b92939c05f0cd1
jw <105700413+listenai-jw@users.noreply.github.com>
2023-10-27 19:19:08 +0800

    v2.0.0 publish

--- main.mjs
+++ main.mjs
@@ -3,7 +3,7 @@
 let manifest = await fs.readFile('./manifest.json');
 manifest = JSON.parse(manifest.toString());
 
-await $`lisa install @lisa-plugin/zephyr@1.7.2 -g  --registry=https://registry-lpm.listenai.com `
+await $`lisa install @lisa-plugin/zephyr@2.0.0 -g  --registry=https://registry-lpm.listenai.com `
 await $`lisa install @lisa-plugin/term@1.1.0 -g  --registry=https://registry-lpm.listenai.com `
 await $`lisa zep use-env --clear`
 await $`lisa zep use-env csk6 `
