f4dbafd8839e474ac693506e5a40fcf57e2e4482
chh7872 <105700413+chh7872@users.noreply.github.com>
2025-02-10 17:24:38 +0800

    update lisa entry script

--- standalone/pwsh/lisa.cmd
+++ standalone/pwsh/lisa.cmd
@@ -1,1 +1,2 @@
+set npm_config_prefix=%LISA_PREFIX%
 "%LISA_PREFIX%\node" "%LISA_PREFIX%\node_modules\@listenai\lisa\bin\run" %*
--- standalone/pwsh/lisa.ps1
+++ standalone/pwsh/lisa.ps1
@@ -1,2 +1,3 @@
 #!/usr/bin/env pwsh
+$env:npm_config_prefix=$env:LISA_PREFIX
 & "$env:LISA_PREFIX\node.exe" "$env:LISA_PREFIX\node_modules\@listenai\lisa\bin\run" $args
